a7d5b132bfdf5cbf7fc24a916390fb6a5b5bfcfb
= <=>
2022-10-08 21:08:51 +0700

    add firework js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
-var x = document.getElementById("audio");
-	const boxM = document.getElementById('btn');
-
-	boxM.addEventListener('click',(event)=>{
-		setTimeout(function box(){
-			boxM.style.display = "none";
-
-			Swal.fire({
-			  imageUrl: 'https://asset.kompas.com/crops/hCsl_4aqNq0wTSM25eLta8dsGPU=/5x4:1000x667/750x500/data/photo/2020/11/21/5fb8e8551ad37.jpg',
-			  imageHeight: 300,
-			  imageAlt: 'A tall image'
+	$(document).ready(function(){
+		$("#btn").click(function(){
+			$("#present").fadeOut(10000, "swing", function(){
+				Swal.fire({
+				  imageUrl: 'https://asset.kompas.com/crops/hCsl_4aqNq0wTSM25eLta8dsGPU=/5x4:1000x667/750x500/data/photo/2020/11/21/5fb8e8551ad37.jpg',
+				  imageHeight: 300,
+				  imageAlt: 'A tall image',
+				  timer:2000
+				})	
 			})
-		}, 5000)
+
+		})
 	})
+
+	var x = document.getElementById("audio");
 
 	function audio(){
 		x.play();
@@ -25,3 +26,14 @@ var x = document.getElementById("audio");
 
 		fireworks.start();
 	}
+
+	var warning = ['Jangan Lupa Mandi Yah !', 'Jangan Lupa Makan Yah !', 'Jangan Lupa Shallat Yah !', 'Jangan Lupa Pikirin Aku Yah !'];
+function myWarning(ok) {
+          for(var i=0;i<warning.length;i++) {
+              setTimeout(alert(warning[i]), 1000);
+          }
+	         }
+
+
+myWarning(warning);
+
